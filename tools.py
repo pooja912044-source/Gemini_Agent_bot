4b3910dbd1a79ae3bdf0cd755ee5e18f268ff261
pooja912044 <pooja912044@gmail.com>
2026-06-27 19:24:18 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,1 +1,13 @@
+from langchain.tools import tool
+@tool
+def calculator(expression:str):
+    """Perform mathematical calculations."""
+    try:
+        return str(eval(expression))
+    except:
+        return "Invalid Expression"
+@tool
+def word_counter(text:str):
+    """Count the number of words in the given text."""
+    return f"Total words: {len(text.split())}"
 
